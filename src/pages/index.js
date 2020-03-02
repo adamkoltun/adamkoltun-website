bc1c464ed5c6f6673215786ae0558a604613174d
Adam Koltun <akoltun4@gmail.com>
2020-03-02 21:47:48 +0100

    delete nav after include layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import React from "react"
 
-import Navigation from "../components/Navigation/Navigation"
-
 const IndexPage = () => (
   <>
-    <Navigation />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
