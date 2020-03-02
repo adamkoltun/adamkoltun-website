335f2c4c99da048bfc8e754948ca9945cb86d565
Adam Koltun <akoltun4@gmail.com>
2020-03-02 19:50:05 +0100

    add new pages

---
+++ src/pages/portfolio.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const PortfolioPage = () => (
+  <Layout>
+    <h2>Portfolio page</h2>
+  </Layout>
+)
+
+export default PortfolioPage
