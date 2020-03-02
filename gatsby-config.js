9fffe9ee74e4e1fa368a6ea2bce9d525b2dff901
Adam Koltun <akoltun4@gmail.com>
2020-03-02 21:46:04 +0100

    install gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,15 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    `gatsby-plugin-layout`,
     {
       resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
@@ -27,6 +19,15 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     // // {
     // //   resolve: `gatsby-plugin-manifest`,
     // //   options: {
