d900060092b30b34a3f367ef829026d8ba9e0ebd
Adam Koltun <akoltun4@gmail.com>
2020-03-27 15:44:42 +0100

    add header for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import styled from "styled-components"
 
+import Header from "../components/organisms/Header/Header"
+
 const StyledPageWrapper = styled.div`
-  height: 200vh;
+  height: 100vh;
 `
 
 const IndexPage = () => (
   <StyledPageWrapper>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Header />
   </StyledPageWrapper>
 )
 
