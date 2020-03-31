407c3ab1cc93c76b3915241bf2922f9f9030b8c1
Adam Koltun <akoltun4@gmail.com>
2020-03-31 16:44:09 +0200

    animate mobile ux arrow

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -36,10 +36,38 @@ const StyledHeading = styled.h1`
   }
 `
 
-const StyledMobileArrow = styled(MobileArrow)`
-  width: 20px;
+const StyledMobileArrowWrapper = styled.div`
   justify-self: center;
   align-self: center;
+`
+
+const StyledMobileArrow = styled(MobileArrow)`
+  width: 20px;
+  animation: scrollDown 1.5s infinite linear both;
+
+  @keyframes scrollDown {
+    0% {
+      transform: translateY(-25%);
+      opacity: 0;
+    }
+
+    25% {
+      opacity: 0.3;
+    }
+
+    50% {
+      opacity: 1;
+    }
+
+    75% {
+      opacity: 0.3;
+    }
+
+    100% {
+      transform: translateY(125%);
+      opacity: 0;
+    }
+  }
 `
 
 const headings = [
@@ -89,7 +117,9 @@ const Header = () => {
     <StyledHeaderWrapper>
       <StyledHeaderSvg />
       <StyledHeading ref={headingRef}>{heading}</StyledHeading>
-      <StyledMobileArrow />
+      <StyledMobileArrowWrapper>
+        <StyledMobileArrow />
+      </StyledMobileArrowWrapper>
     </StyledHeaderWrapper>
   )
 }
