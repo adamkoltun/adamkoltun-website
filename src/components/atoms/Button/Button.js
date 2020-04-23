096c5c60fb8c89c1720171efd7c7d601b2a3eefc
Adam Koltun <akoltun4@gmail.com>
2020-04-23 17:33:20 +0200

    update button component

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,8 +1,14 @@
 import React from "react"
-import styled from "styled-components"
+import { Link } from "gatsby"
+import styled, { css } from "styled-components"
+
+const ButtonContainer = styled.div`
+  display: flex;
+`
 
 const StyledButton = styled.button`
   display: flex;
+  width: auto;
   align-items: center;
   border: none;
   background-color: transparent;
@@ -11,11 +17,18 @@ const StyledButton = styled.button`
   color: #292929;
   text-transform: uppercase;
 
-  :hover :first-child {
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      color: #fff;
+      margin-top: 75px;
+    `};
+
+  :hover span:nth-child(2) {
     transform: scaleX(0.4);
   }
 
-  :hover :last-child {
+  :hover span:last-child {
     transform: translateX(-50%);
   }
 `
@@ -28,21 +41,32 @@ const StyledLine = styled.span`
   background-color: #292929;
   transform-origin: 0% 50%;
   transition: transform 0.6s ease;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: #fff;
+    `};
 `
 
 const StyledWord = styled.span`
   transition: transform 0.6s ease;
 `
 
-const Button = ({ children }) => {
+const Button = ({ children, destination, secondary }) => {
   const words = children.split(" ")
 
   return (
-    <StyledButton>
-      {words[0]}
-      <StyledLine></StyledLine>
-      <StyledWord>{words[1]}</StyledWord>
-    </StyledButton>
+    <ButtonContainer>
+      <Link to={destination}>
+        <StyledButton secondary={secondary}>
+          <span>{words[0]}</span>
+          <StyledLine secondary={secondary}></StyledLine>
+          <StyledWord>{words[1]}</StyledWord>
+        </StyledButton>
+      </Link>
+      <div></div>
+    </ButtonContainer>
   )
 }
 
