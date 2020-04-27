a64e164d1d9bb3bbebf440e418a060c0adff6864
Adam Koltun <akoltun4@gmail.com>
2020-04-27 15:38:08 +0200

    add footer to layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import GlobalStyle from "../assets/styles/globalStyles"
 
 import TopBar from "../components/organisms/TopBar/TopBar"
+import Footer from "../components/organisms/Footer/Footer"
 
 const MainLayout = ({ children }) => (
   <>
     <GlobalStyle />
     <TopBar />
     {children}
+    <Footer />
   </>
 )
 
